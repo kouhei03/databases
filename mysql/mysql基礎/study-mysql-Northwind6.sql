be3c7410fcc8b82d05f7443a3c566b51d79be114
kouhei takahashi <kouhei@restablished.com>
2021-12-02 08:21:13 +0900

    q

---
+++ mysql/mysql基礎/study-mysql-Northwind6.sql
@@ -0,0 +1,35 @@
+
+/*SQL算術演算子*/
+SELECT 30 + 20;
+SELECT 30 - 20;
+SELECT 30 * 20;
+SELECT 30 / 10;
+SELECT 17 % 5;
+
+/*SQL比較演算子*/
+SELECT * FROM Product
+WHERE unitPrice = 18;
+
+SELECT * FROM Product
+WHERE unitPrice > 30;
+
+SELECT * FROM Product
+WHERE unitPrice < 30;
+
+SELECT * FROM Product
+WHERE unitPrice >= 30;
+
+SELECT * FROM Product
+WHERE unitPrice <= 30;
+
+SELECT * FROM Product
+WHERE unitPrice <> 30;
+
+/*SQL論理演算子*/
+
+
+/**/
+/**/
+/**/
+/**/
+/**/
